506dc51b69ed65b02066c06857618f415b23ff67
Zeyu Lin <382651746@qq.com>
2017-09-05 17:53:16 +0800

    Implement the logic in php #94
    Use the variable OrderBy to record the order that user chooses:
    alphabetical, descending, ascending.

--- Website/dashboard/SubmissionTimesOfEachStudent.php
+++ Website/dashboard/SubmissionTimesOfEachStudent.php
@@ -1,22 +1,37 @@
 <?php
 include_once('../one_connection.php');
 
+//$OrderBy records the order: alphabetical, descending, ascending
+$order =2;
+$OrderBy='';
+switch ($order)
+{
+case 1:
+  $OrderBy='';
+  break;
+case 2:
+  $OrderBy='ORDER BY count desc';
+  break;
+case 3:
+  $OrderBy='ORDER BY count asc';
+  break;
+}
+
 $sql = "SELECT FKUserId, COUNT(  `Id` ) count
-FROM event
-WHERE DataSourceType=2
-GROUP BY FKUserId";
+from event
+where `CourseName`='MSE' and `SchoolYear`='2012' and `Semester`='Semester 2' and `AssignmentName`='Assignment 2' and `DataSourceType`=2 and `FKEventTypeId`=6
+GROUP BY FKUserId
+HAVING count {$OrderBy}";
+
 $query = mysql_query($sql);
 while($row=mysql_fetch_array($query)){
 	$arr[] = array(
 		'FKUserId'=> $row['FKUserId'],
 		'count' => $row['count']
-
 	);
 }
-//var_dump($arr);
-
 
 mysql_close($link);
 echo json_encode($arr);
-//[{"day":"20170304","count":"5"},{"name":"�½�","value":"0.94"}]
+//[{"FKUserId":"21685","count":"5"},{"FKUserId":"21687","count":"6"}]
 ?>
