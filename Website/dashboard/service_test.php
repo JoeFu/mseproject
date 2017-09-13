91a79419f76c9c1a44de2b223ba222e73ff7cabb
Zeyu Lin <382651746@qq.com>
2017-09-13 12:51:19 +0930

    Unit test for LoadCourse, LoadYear #74

---
+++ Website/dashboard/service_test.php
@@ -0,0 +1,55 @@
+<?php 
+require_once dirname(__FILE__).'./service_class.php';
+
+//Development environment: WAMPServer Version 2.2 (PHP 5.3.13, MYSQL 5.5.24)
+//Testing tool: PhpUnit4.8.36
+
+class ServiceTest extends PHPUnit_Framework_TestCase 
+{
+	public function testLoadCourse() 
+	{
+		//correct expected value
+		$expected = '[{"CourseName":"Distributed Systems"},{"CourseName":"MSE"}]';
+		$service = new Service;
+		$actual = $service->loadCourse();
+		$this->assertEquals($expected,$actual);
+
+		//incorrect expected value
+		$expected = 'false';
+		$service = new Service;
+		$actual = $service->loadCourse();
+		$this->assertNotEquals($expected,$actual);
+	}
+
+	public function testLoadYear() 
+	{
+		//correct expected value for course "MSE"
+		$expected = '[{"SchoolYear":"2012"},{"SchoolYear":"2013"}]';
+		$SelectCourseId="MSE";
+		$service = new Service;
+		$actual = $service->loadYear($SelectCourseId);
+		$this->assertEquals($expected,$actual);
+
+		//correct expected value for course "Distributed Systems"
+		$expected = '[{"SchoolYear":"2012"},{"SchoolYear":"2013"},{"SchoolYear":"2014"}]';
+		$SelectCourseId="Distributed Systems";
+		$service = new Service;
+		$actual = $service->loadYear($SelectCourseId);
+		$this->assertEquals($expected,$actual);
+
+		//incorrect expected value for course "MSE"
+		$expected = 'false';
+		$SelectCourseId="MSE";
+		$service = new Service;
+		$actual = $service->loadYear($SelectCourseId);
+		$this->assertNotEquals($expected,$actual);
+
+		//incorrect expected value for course "Distributed Systems"
+		$expected = 'false';
+		$SelectCourseId="Distributed Systems";
+		$service = new Service;
+		$actual = $service->loadYear($SelectCourseId);
+		$this->assertNotEquals($expected,$actual);
+	}
+}
+?>
