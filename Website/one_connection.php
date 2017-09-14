c0ff39bba7f6923200a109463009144af70f2d46
JoeFu <fuzeqi@gmail.com>
2017-09-14 13:19:23 +0930

    update one_connection page fix #120
    remove chinese comment, and reset the databse password.

--- Website/one_connection.php
+++ Website/one_connection.php
@@ -3,16 +3,17 @@
     /**
     * @Database Connection
     */
-    date_default_timezone_set('PRC'); //Australia/Adelaide
-    /**
-     * @数据库链接
-    */
+    date_default_timezone_set('PRC'); 
     $host="localhost";
     $db_user="root";
-    $db_pass="outlook";
+    $db_pass="";
     $db_name="studentdata";
     
     
     $link=mysql_connect($host,$db_user,$db_pass);
     mysql_select_db($db_name,$link);
+    mysql_query("SET names UTF8");
+    
+    header("Content-Type: text/html; charset=utf-8");
+
 ?>
