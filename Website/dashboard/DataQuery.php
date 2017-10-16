3148049815a7de9022edf026d05e5dbd937277ac
unknown <pmtam2010@gmail.com>
2017-10-17 09:09:51 +1030

    fix #143 - Data Query - Implement unit tests

---
+++ Website/dashboard/DataQuery.php
@@ -0,0 +1,19 @@
+<?php
+class DataQuery
+{
+ 
+	public function callAPI($service_url, $curl_post_data)
+    {
+    	$curl = curl_init($service_url);
+    	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    	if ($curl_post_data != null){
+    		curl_setopt($curl, CURLOPT_POST, true);
+    		curl_setopt($curl, CURLOPT_POSTFIELDS, $curl_post_data);    		
+    	}
+    	$curl_response = curl_exec($curl);    	
+    	curl_close($curl);
+    	return $curl_response;
+    	//print_r($curl_response);
+    }
+ 
+}
