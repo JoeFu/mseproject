344d88b4b74f577b01db0d93d7c63329e0ade7f9
JoeFu <fuzeqi@gmail.com>
2017-10-28 12:24:48 +1030

    Update Backend API service and API test #80
    Update Backedn and add API Test

--- Website/demo/backend/APIs.php
+++ Website/demo/backend/APIs.php
@@ -2,55 +2,86 @@
 /** APIs is for new DashBoard, Simple Query and Report System **/
 session_start();
 require_once 'Service.php';
-$service = new Service;
+$service = new APIService;
 $option = $_GET['option'];
 
 //function selection
 if($option == "LoadActivityNumber")
 {
-    $service->LoadActivityNumber();
+    echo $service->LoadActivityNumber();
+
 }
 elseif ($option == "LoadStudentsNumber")
 {
-    $service->LoadStudentsNumber();
+    echo $service->LoadStudentsNumber();
 }
 elseif ($option == "LoadCoursesNumber")
 {
-    $service->LoadCoursesNumber();
+    echo $service->LoadCoursesNumber();
 }
 elseif($option == "LoadCoursesDetail")
 {
-    $service->LoadCoursesDetail();
+    echo $service->LoadCoursesDetail();
 }
 elseif($option == "postName")
 {
-    $service->postName();
+    echo $service->postName();
 }
 elseif($option == "Name")
 {
-    $service->Name();
+    echo $service->Name();
 }
 elseif($option == "LoadCourse")
 {
-    $service->LoadCourse();
+    require_once '../../dashboard/service_class.php';
+    include_once('one_connection.php');
+    $new_service = new Service;
+    echo $new_service->loadCourse();
+
+    // echo $service->LoadCourse();
 }
 elseif($option == "LoadYear")
 {
     $SelectCourseId = $_GET['SelectCourseId'];
-    $service->LoadYear($SelectCourseId);
+    require_once '../../dashboard/service_class.php';
+    include_once('one_connection.php');
+    $new_service = new Service;
+    echo $new_service->loadYear($SelectCourseId);
 }
 elseif($option == "LoadSemester")
 {
     $SelectCourseId = $_GET['SelectCourseId'];
     $SelectYearId = $_GET['SelectYearId'];
-    $service->LoadSemester($SelectCourseId, $SelectYearId);
+    require_once '../../dashboard/service_class.php';
+    include_once('one_connection.php');
+    $new_service = new Service;
+    echo $new_service->loadSemester($SelectCourseId, $SelectYearId);
 }
 elseif($option == "LoadAssignment")
 {
     $SelectCourseId = $_GET['SelectCourseId'];
     $SelectYearId = $_GET['SelectYearId'];
     $SelectSemesterId= $_GET['SelectSemesterId'];
-    $service->LoadAssignment($SelectCourseId, $SelectYearId, $SelectSemesterId);
+    require_once '../../dashboard/service_class.php';
+    include_once('one_connection.php');
+    $new_service = new Service;
+    echo $new_service->loadAssignment($SelectCourseId, $SelectYearId, $SelectSemesterId);
+}
+elseif($option == "SelectYear")
+{
+    echo $service->SelectYear();
+}
+elseif($option == "SearchTable")
+{
+    $SelectCourseId = $_GET['SelectCourseId'];
+    $SelectYearId = $_GET['SelectYearId'];
+    $SelectSemesterId= $_GET['SelectSemesterId'];
+    $SelectAssignmentId= $_GET['SelectAssignmentId'];
+    echo $service->SearchTable($SelectCourseId, $SelectYearId, $SelectSemesterId, $SelectAssignmentId);
+}
+elseif($option == "IndexCharts")
+{
+    echo $service->IndexCharts();
 }
 else
 {
